6cf4c93aeec62300a2c48c9dc8595415df464f79
Jiang Xiachen <102943264+Xiachen-Jiang@users.noreply.github.com>
2024-09-09 19:59:12 +0930

    top bar

---
+++ gpa_predictor_cal.client/src/components/Content/content.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './content.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export function Content(): JSX.Element {
+    return (
+
+        <>
+            <h1>Hello world</h1>
+        </>
+
+    );
+}
